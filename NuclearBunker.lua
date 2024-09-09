6da07a1386b319ba0546bfca913a3e9ff4770847
nvkob1 <nvkob1@gmail.com>
2024-09-09 17:28:43 +0700

    Update NuclearBunker.lua

--- NuclearBunker.lua
+++ NuclearBunker.lua
@@ -9,6 +9,18 @@ local RunService = game:GetService("RunService")
 
 local isEating = false -- To track if the player is currently eating
 
+-- Anti-Afk
+if not _G.AntiAfk then
+    _G.AntiAfk = true
+    game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "Anti-Afk Enabled!", Duration = 5})
+    game.Players.LocalPlayer.Idled:connect(function()
+        game:GetService('VirtualUser'):CaptureController()
+        game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+    end)
+else
+    game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "You Already Executed!", Duration = 5})
+end
+
 -- Enable noclip
 local function noclip()
     local character = player.Character
