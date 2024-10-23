832685f0f75adac75c8dd05b5146967a93364e75
nvkob1 <nvkob1@gmail.com>
2024-10-23 12:39:30 +0700

    Update main.lua

--- Player-Based Server Hopper/main.lua
+++ Player-Based Server Hopper/main.lua
@@ -1,31 +1,33 @@
-local Players = game:GetService("Players")
-local HttpService = game:GetService("HttpService")
-local TeleportService = game:GetService("TeleportService")
+return function(minimumPlayers)
+    local Players = game:GetService("Players")
+    local HttpService = game:GetService("HttpService")
+    local TeleportService = game:GetService("TeleportService")
 
--- Function to hop servers
-local function hopServer()
-    local servers = {}
-    local gameId = game.PlaceId
-    local url = "https://games.roblox.com/v1/games/" .. gameId .. "/servers/Public?sortOrder=Asc&limit=100"
+    -- Function to hop servers
+    local function hopServer()
+        local servers = {}
+        local gameId = game.PlaceId
+        local url = "https://games.roblox.com/v1/games/" .. gameId .. "/servers/Public?sortOrder=Asc&limit=100"
 
-    local response = HttpService:JSONDecode(game:HttpGet(url))
-    
-    if response and response.data then
-        for _, server in pairs(response.data) do
-            if server.playing and server.playing < server.maxPlayers then
-                table.insert(servers, server.id)
+        local response = HttpService:JSONDecode(game:HttpGet(url))
+
+        if response and response.data then
+            for _, server in pairs(response.data) do
+                if server.playing and server.playing < server.maxPlayers then
+                    table.insert(servers, server.id)
+                end
             end
+        end
+
+        -- Hop to a random server
+        if #servers > 0 then
+            local serverToJoin = servers[math.random(1, #servers)]
+            TeleportService:TeleportToPlaceInstance(gameId, serverToJoin, Players.LocalPlayer)
         end
     end
 
-    -- Hop to a random server
-    if #servers > 0 then
-        local serverToJoin = servers[math.random(1, #servers)]
-        TeleportService:TeleportToPlaceInstance(gameId, serverToJoin, Players.LocalPlayer)
+    -- Check if the total number of players (including yourself) equals the minimumPlayers
+    if #Players:GetPlayers() == minimumPlayers then
+        hopServer()
     end
-end
-
--- Check if the total number of players (including yourself) equals the minimumPlayers
-if #Players:GetPlayers() == minimumPlayers then
-    hopServer()
 end
