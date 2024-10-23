fe65f638673b2d87fcf8355f23f6a9c8302ea88f
nvkob1 <nvkob1@gmail.com>
2024-10-23 12:39:58 +0700

    Update loadstring.lua

--- Player-Based Server Hopper/loadstring.lua
+++ Player-Based Server Hopper/loadstring.lua
@@ -1,2 +1,2 @@
 local minimumPlayers = 2
-loadstring(game:HttpGet('https://github.com/nvkob1/rbxscripts/raw/refs/heads/main/Player-Based%20Server%20Hopper/main.lua'))()
+loadstring(game:HttpGet('https://github.com/nvkob1/rbxscripts/raw/refs/heads/main/Player-Based%20Server%20Hopper/main.lua'))(minimumPlayers)
