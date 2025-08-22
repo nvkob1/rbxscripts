10036a1ee0b0ae954b3570d63668697026b6406d
nvkob1 <nvkob1@gmail.com>
2025-08-22 18:04:41 +0700

    Update and rename fixed.lua to remake.lua

--- FloodEscape2/remake.lua
+++ FloodEscape2/remake.lua
@@ -1,5 +1,5 @@
 -- Auto Farm created by tomato.txt on discord
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))();task.spawn(loadstring(game:HttpGet("https://raw.githubusercontent.com/tomatotxt/Flood-GUI/refs/heads/main/win")))
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))();task.spawn(loadstring(game:HttpGet("https://raw.githubusercontent.com/nvkob1/rbxscripts/refs/heads/main/FloodEscape2/autofarm.lua")))
 local Window = Library.CreateLib("WinIt x1000", "Ocean")
 local Tab = Window:NewTab("Auto")
 local Section = Tab:NewSection("Main")
