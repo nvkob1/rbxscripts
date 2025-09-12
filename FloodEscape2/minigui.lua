2732e17a24da505014b82692a916f02cda3e78fd
nvkob1 <nvkob1@gmail.com>
2025-09-12 22:02:11 +0700

    Delete FloodEscape2/minigui.lua

--- FloodEscape2/minigui.lua
+++
@@ -1,8 +0,0 @@
--- Auto Farm created by tomato.txt on discord
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))();task.spawn(loadstring(game:HttpGet("https://raw.githubusercontent.com/nvkob1/rbxscripts/refs/heads/main/FloodEscape2/autofarm.lua")))
-local Window = Library.CreateLib("WinIt x1000", "Ocean")
-local Tab = Window:NewTab("Auto")
-local Section = Tab:NewSection("Main")
-Section:NewToggle("Auto-Farm", "Wins for your noob self!", function(state)
-    getgenv().TomatoAutoFarm = state
-end);Section:NewLabel("made by tomato.txt")
