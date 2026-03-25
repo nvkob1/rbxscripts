52dae7f64ad7765946257389bbbcff00b9e1bd4f
nvkob1 <nvkob1@gmail.com>
2026-03-25 20:37:52 +0700

    Update ServerHopper.lua

--- FNAF_TPRR_AutoFarm/ServerHopper.lua
+++ FNAF_TPRR_AutoFarm/ServerHopper.lua
@@ -1,34 +1,101 @@
 local folderName = "TPRR Auto Farm"
 if not isfolder(folderName) then makefolder(folderName) end
 
-local HttpService = game:GetService("HttpService")
-local TeleportService = game:GetService("TeleportService")
-local Players = game:GetService("Players")
-local PlaceId = game.PlaceId
-local JobId = game.JobId
+local API, HttpService, TeleportService, CoreGui = nil, game:GetService("HttpService"), game:GetService("TeleportService"), game:GetService("CoreGui")
+local RemoveErrorPrompts = true
+local IterationSpeed = 0.25
+local ExcludefullServers = true
+local SaveTeleportAttempts = false
+local API = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?limit=100"
 
-local function serverHop()
-    local servers = {}
-    local s, req = pcall(function()
-        return game:HttpGet("https://games.roblox.com/v1/games/" .. PlaceId .. "/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true")
+local serversFile = folderName .. "/Servers.JSON"
+local attemptsFile = folderName .. "/Attempts.txt"
+
+local function EncodeToFile(JSONString)
+    local success, JSONData = pcall(function()
+        return HttpService:JSONDecode(JSONString)
     end)
-    if not s or not req then warn("Failed to fetch servers.") return end
-
-    local body = HttpService:JSONDecode(req)
-    if body and body.data then
-        for _, v in next, body.data do
-            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers)
-                and v.playing < v.maxPlayers and v.id ~= JobId then
-                table.insert(servers, 1, v.id)
-            end
+    if success and JSONData.data then
+        JSONData.gameId = game.PlaceId
+        local s, encoded = pcall(function()
+            return HttpService:JSONEncode(JSONData)
+        end)
+        if s then
+            writefile(serversFile, encoded)
+        else
+            warn("Failed to encode JSON string.")
+            return nil
         end
-    end
-
-    if #servers > 0 then
-        TeleportService:TeleportToPlaceInstance(PlaceId, servers[math.random(1, #servers)], Players.LocalPlayer)
     else
-        warn("Serverhop: Couldn't find a server.")
+        warn("Failed to decode JSONData.")
+        return nil
+    end
+    return JSONData
+end
+
+local function NextCursor(ep)
+    return game:HttpGet(API .. "&excludeFullGames=" .. tostring(ExcludefullServers) .. ((ep and "&cursor=" .. ep) or ""))
+end
+
+local function StartTeleport()
+    local JSONData = EncodeToFile(readfile(serversFile))
+    if not JSONData then
+        writefile(serversFile, game:HttpGet(API))
+        StartTeleport()
+    end
+    for i = 0, 99 do
+        if #JSONData.data <= 1 then
+            EncodeToFile(NextCursor(JSONData.nextPageCursor))
+            TeleportService:Teleport(game.PlaceId, game.Players.LocalPlayer)
+        end
+        if JSONData.data[i] then
+            local JobId = JSONData.data[i].id
+            table.remove(JSONData.data, i)
+            local s, encoded = pcall(function()
+                return HttpService:JSONEncode(JSONData)
+            end)
+            writefile(serversFile, encoded)
+            if SaveTeleportAttempts then
+                appendfile(attemptsFile, JobId .. "\n")
+            end
+            TeleportService:TeleportToPlaceInstance(game.PlaceId, JobId, game.Players.LocalPlayer)
+            task.wait(IterationSpeed)
+        end
     end
 end
 
-serverHop()
+local function SetMainPage()
+    writefile(serversFile, game:HttpGet(API))
+    StartTeleport()
+end
+
+if RemoveErrorPrompts then
+    CoreGui:WaitForChild("RobloxGui"):WaitForChild("Modules"):WaitForChild("ErrorPrompt"):Destroy()
+    CoreGui.RobloxPromptGui:Destroy()
+end
+
+if isfile(serversFile) then
+    local success, JSONData = pcall(function()
+        return HttpService:JSONDecode(readfile(serversFile))
+    end)
+    if success and JSONData then
+        if JSONData.gameId ~= game.PlaceId then
+            warn("Game mismatch, remaking cache for --> " .. game.PlaceId)
+            SetMainPage()
+        end
+        if JSONData.data and #JSONData.data >= 1 then
+            StartTeleport()
+        else
+            if success and JSONData.nextPageCursor then
+                EncodeToFile(NextCursor(JSONData.nextPageCursor))
+                StartTeleport()
+            else
+                SetMainPage()
+            end
+        end
+    else
+        SetMainPage()
+    end
+else
+    SetMainPage()
+end
