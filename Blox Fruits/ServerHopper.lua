3e882f18133b7a371cd29fc9ea68c6ea45efb5fd
nvkob1 <nvkob1@gmail.com>
2026-07-05 21:14:13 +0700

    Update ServerHopper.lua

--- Blox Fruits/ServerHopper.lua
+++ Blox Fruits/ServerHopper.lua
@@ -3,7 +3,6 @@ getgenv().ServerHopConfig = getgenv().ServerHopConfig or {
     LowPlayers = true
 }
 local ServerBrowser = game:GetService("ReplicatedStorage"):WaitForChild("__ServerBrowser")
-local RemoveErrorPrompts = true
 
 function ServerHop()
     local config = getgenv().ServerHopConfig
@@ -59,8 +58,6 @@ function ServerHop()
         print(("[ServerHop] Attempt %d: Joining Job: %s | Region: %s | Players: %s"):format(
             attempt, target.Job, tostring(target.Region), tostring(target.Count)
         ))
-
-        if RemoveErrorPrompts then CoreGui:WaitForChild("RobloxGui"):WaitForChild("Modules"):WaitForChild("ErrorPrompt"):Destroy() CoreGui.RobloxPromptGui:Destroy() end
 
         local ok = pcall(function() ServerBrowser:InvokeServer("teleport", target.Job) end)
         if ok then
