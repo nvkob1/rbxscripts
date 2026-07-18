2b1c04a317e18ae517b1e6af9d3ec74520302c62
nvkob1 <nvkob1@gmail.com>
2026-07-18 09:01:55 +0700

    Create loader.lua

---
+++ +1MuscletoBreakWalls/loader.lua
@@ -0,0 +1,23 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nvkob1/rbxscripts/refs/heads/main/UNC_Check/UNC-Check.lua"))()({
+    scriptName = "MuscletoBreakWalls",
+    requiredFunctions = {
+        "loadstring",
+        "game.HttpGet",
+        "isfolder",
+        "makefolder",
+        "writefile",
+        "isfile",
+        "getgenv",
+        "task.spawn",
+        "task.wait",
+        "firetouchinterest"
+    },
+    optionalFunctions = {
+        "cloneref",
+        "hookmetamethod",
+        "getrawmetatable"
+    },
+    mainScript = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/nvkob1/rbxscripts/refs/heads/main/%2B1MuscletoBreakWalls/script.lua"))()
+    end
+})
