e5e605aed673b8eab495243723624d3570fbdaa1
nvkob1 <nvkob1@gmail.com>
2026-08-07 14:56:26 +0700

    Update lates-lib-source.lua

--- modified-lates-lib/lates-lib-source.lua
+++ modified-lates-lib/lates-lib-source.lua
@@ -205,7 +205,7 @@ end
 --// Setup [UI]
 if (identifyexecutor) then
 	Screen = Services.Insert:LoadLocalAsset("rbxassetid://18490507748");
-	Blur = loadstring(game:HttpGet("https://raw.githubusercontent.com/lxte/lates-lib/main/Assets/Blur.lua"))();
+	Blur = loadstring(game:HttpGet("https://raw.githubusercontent.com/nvkob1/rbxscripts/refs/heads/main/modified-lates-lib/Assets/blur.lua"))();
 else
 	Screen = (script.Parent);
 	Blur = require(script.Blur)
